2ed6ef71af2b22e35717316993f2cf80cfa3979b
Mark Koren <mkoren@stanford.edu>
2019-11-20 15:01:55 -0800

    added stargan data gen

--- data_gen.py
+++ data_gen.py
@@ -17,6 +17,7 @@ from waymo_open_dataset import dataset_pb2 as open_dataset
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+import json
 import pdb
 
 
@@ -66,7 +67,7 @@ def image_test():
             plt.show()
             pdb.set_trace()
 
-def generate_dataset(path_to_trainings, path_to_data):
+def generate_dataset_pixelcnn(path_to_trainings, path_to_data):
     for dirname in os.listdir(path_to_trainings):
         if 'training_' in dirname:
             data_type = '/training'
@@ -95,10 +96,64 @@ def generate_dataset(path_to_trainings, path_to_data):
         
         # img_small = tf.image.resize(img, (32,32), method=ResizeMethod.BILINEAR)
         # plot_image(img_small)
-        
+
+def generate_dataset_stargan(path_to_labels, path_to_images, path_to_data, max_image_count = np.inf):
+    with open(join(path_to_labels, 'bdd100k_labels_images_train.json')) as f:
+        data = json.load(f)
+    
+    for img_index, img_data in enumerate(data):
+        if img_index >= max_image_count:
+            break
+
+        img_path = join(path_to_images, 'train', img_data['name'])
+        with Image.open(img_path) as img:
+            img_small = img.resize((128,72),resample=Image.LANCZOS)
+
+            weather_type = img_data['attributes']['weather']
+            time_of_day = img_data['attributes']['timeofday']
+
+            if not os.path.exists(join(path_to_data, 'train', weather_type)):
+                os.makedirs(join(path_to_data, 'train', weather_type))
+            img_small.save(join(path_to_data, 'train', weather_type, img_data['name']))
+
+            if not os.path.exists(join(path_to_data, 'train', time_of_day)):
+                os.makedirs(join(path_to_data, 'train', time_of_day))
+            img_small.save(join(path_to_data, 'train', time_of_day, img_data['name']))
+
+    with open(join(path_to_labels, 'bdd100k_labels_images_val.json')) as f:
+        data = json.load(f)
+    
+    for img_index, img_data in enumerate(data):
+        if img_index >= max_image_count:
+            break
+
+        img_path = join(path_to_images, 'val', img_data['name'])
+        with Image.open(img_path) as img:
+            img_small = img.resize((128,72),resample=Image.LANCZOS)
+
+            weather_type = img_data['attributes']['weather']
+            time_of_day = img_data['attributes']['timeofday']
+
+            if not os.path.exists(join(path_to_data, 'test', weather_type)):
+                os.makedirs(join(path_to_data, 'test', weather_type))
+            img_small.save(join(path_to_data, 'test', weather_type, img_data['name']))
+
+            if not os.path.exists(join(path_to_data, 'test', time_of_day)):
+                os.makedirs(join(path_to_data, 'test', time_of_day))
+            img_small.save(join(path_to_data, 'test', time_of_day, img_data['name']))
+
+
+
+
         
 # image_test()
-generate_dataset('/home/mkoren/scratch/Research/cs236project/deep_image_falsification_for_av',
-    '/home/mkoren/scratch/Research/cs236project/data/av_96_64')
+# generate_dataset('/home/mkoren/scratch/Research/cs236project/deep_image_falsification_for_av',
+#     '/home/mkoren/scratch/Research/cs236project/data/av_96_64')
+# '/home/mkoren/Downloads/bdd100k/labels/bdd100k_labels_images_train.json'
+generate_dataset_stargan(path_to_labels = '/home/mkoren/scratch/Research/cs236project/data/bdd100k/labels',
+                         path_to_images = '/home/mkoren/scratch/Research/cs236project/data/bdd100k/images/100k',
+                         path_to_data = '/home/mkoren/scratch/Research/cs236project/data/bdd100k_stargan',
+                         # max_image_count = 100)
+                         )
 # pdb.set_trace()
 print("------- end of script --------")
